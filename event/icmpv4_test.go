391e001cd1a61cce87f43505efe7bbcb0d987383
Junki Kaneko <theoden9014@gmail.com>
2020-01-23 16:31:42 +0900

    enable to run test in event/icmpv4

--- event/icmpv4_test.go
+++ event/icmpv4_test.go
@@ -1,11 +1,18 @@
 package event_test
 
-// func TestICMPv4(t *testing.T) {
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
+import (
+	"context"
+	"testing"
 
-// 	ev := event.NewICMPv4Event(1, "0.0.0.0", "google.com")
-// 	if err := ev.Fire(ctx); err != nil {
-// 		t.Errorf("failed to send icmp from 0.0.0.0 to google.com: %v", err)
-// 	}
-// }
+	"github.com/go-loadtest/evbundler/event"
+)
+
+func TestICMPv4(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ev := event.NewICMPv4Event(1, "0.0.0.0", "google.com")
+	if err := ev.Fire(ctx); err != nil {
+		t.Errorf("failed to send icmp from 0.0.0.0 to google.com: %v", err)
+	}
+}
